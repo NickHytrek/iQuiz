7b98b26adb38d44c89a1b9be51d99e54b243e21b
Nick Hytrek <nshytrek@gmail.com>
2019-02-19 17:22:37 -0800

    edited table setup, working on adding images

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -8,48 +8,33 @@
 
 import UIKit
 
-class QuizDataSource : NSObject, UITableViewDataSource {
-    
-    var titles : [String] = []
-    var subtitles : [String] = []
-    init(_ elements : [String], _ otherElements : [String]) {
-        titles = elements
-        subtitles = otherElements
-    }
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        assert(section == 0)
-        return "Quizzes"
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return titles.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "QuizMath")!
-        
-        cell.textLabel?.text = titles[0]
-        
-        return cell
-    }
-    
-    
-}
 
-
-class ViewController: UIViewController, UITableViewDelegate {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    
     
     @IBOutlet weak var tableView: UITableView!
     
-    let quizzesTitle = [ "Mathematics", "Marvel Super Heroes", "Science"]
-    let quizzesSubtitle = [ "Can you even add 2 and 2?", "How well do you know Deadpool?",
-                            "What chemicals can you mix to create explosions?!"]
+    
+    let quizTitle : [String] = ["Mathematics", "Marvel Super Heroes", "Science"]
+    let quizSubtitle: [String] = ["Can you even add 2 and 2?", "How well do you know Deadpool?", "What chemicals can you mix to create explosions?!"]
+    let quizImages : [UIImage] = [math.jpg, hero.png, science.png]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return quizTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "quizCell", for: indexPath)
+        myCell.textLabel?.text = quizTitle[indexPath.row]
+        return myCell
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     @IBAction func toolBarSettings(_ sender: UIBarButtonItem) {
